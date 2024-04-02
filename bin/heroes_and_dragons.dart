c6be556ae3f1ae9b0f90bd3ccf38142d04ffb00e
Alexey Taradoyna <AltarNatsev@yandex.ru>
2024-04-02 19:27:19 +0700

    init: Initial commit

---
+++ bin/heroes_and_dragons.dart
@@ -0,0 +1,5 @@
+import 'package:heroes_and_dragons/heroes_and_dragons.dart' as heroes_and_dragons;
+
+void main(List<String> arguments) {
+  print('Hello world: ${heroes_and_dragons.calculate()}!');
+}
---
+++ test/heroes_and_dragons_test.dart
@@ -0,0 +1,8 @@
+import 'package:heroes_and_dragons/heroes_and_dragons.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
