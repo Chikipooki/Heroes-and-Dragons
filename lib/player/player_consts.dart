0284991b0191e5dfa8c575135749bf3d0852e327
Alexey Taradoyna <AltarNatsev@yandex.ru>
2024-04-03 10:55:17 +0700

    refactor: Organized the file

--- lib/player/player_consts.dart
+++ lib/player/player_consts.dart
@@ -1,11 +1,14 @@
+//Player
 const String playerName = 'player_name';
 const String playerClass = 'player_class';
+//Class
+const String warriorClass = 'Warrior';
+const String magicClass = 'Magic';
+const String paladinClass = 'Paladin';
+//Stats
 const String power = 'power';
 const String magic = 'magic';
 const String protection = 'protection';
 const String lives = 'lives';
 const String mana = 'mana';
 const String spells = 'spells';
-const String warriorClass = 'Warrior';
-const String magicClass = 'Magic';
-const String paladinClass = 'Paladin';
