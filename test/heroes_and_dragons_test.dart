1c651a0bbd44294fbc021cd256baaf48e104fd33
Alexey Taradoyna <AltarNatsev@yandex.ru>
2024-04-03 10:51:30 +0700

    feat: Add main menu and classes of heroes

--- test/heroes_and_dragons_test.dart
+++ test/heroes_and_dragons_test.dart
@@ -1,8 +1,5 @@
-import 'package:heroes_and_dragons/heroes_and_dragons.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  test('calculate', () {});
 }
